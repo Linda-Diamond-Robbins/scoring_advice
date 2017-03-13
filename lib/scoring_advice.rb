16fa923194d5a693260224fa8244a48a2832e8df
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2017-03-13 18:56:17 -0500

    added docs and spec

--- lib/scoring_advice.rb
+++ lib/scoring_advice.rb
@@ -5,22 +5,7 @@ module ScoringAdvice
   
   extend self
 
-   # Create a HTML unordered list from an array
-  #
-  # @param array [Array]
-  # @return [String]
-  #
-  # @example
-  #   Listless.ul ['foo', 'bar'] #=> "<ul><li>foo</li><li>bar</li></ul>"
-
-  def ul(array)
-    list = array.map { |x| tag(:li, escape(x)) }.join
-    tag(:ul, list)
-  end
-
-  …
-
-end
+  
 
   def ul(array)
     list = array.map { |x| tag(:li, escape(x)) }.join
