e3162672a2a7f29bef5f4fa0333a8336bcaa8002
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2017-03-16 22:01:12 -0500

    fixed switched files

--- lib/scoring_advice.rb
+++ lib/scoring_advice.rb
@@ -1,63 +1,17 @@
-# -*- encoding: utf-8 -*-
-require_relative '../lib/scoring_advice_two'
+require "scoring_advice_two/version"
+require "unirest"
+require 'pry'
 
-describe ScoringAdvice do
-  describe "#get_scoring_advice" do
+module ScoringAdvice
+    extend self
 
-    it"Expect sucessful call and have the desired data returned" do
-      allow(Unirest).to receive(:get).and_return(status_200)
-      desired = {"propensity": 0.26532, "ranking": "C"}
-      expect(ScoringAdviceTwo.get_scoring_advice(10000, 60657, 48)).to eq(desired)
+    def get_scoring_advice(income, zipcode, age)
+      response = Unirest.get("https://not_real.com/customer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}")
+
+      if response[:code] != 200
+        "Something went wrong, #{response[:body][:status_message]}"
+      else
+        response[:body]
+      end
     end
-
-    it"Expect a Bad Request string" do
-      allow(Unirest).to receive(:get).and_return(status_400)
-      desired = "Something went wrong, Bad Request."
-      expect(ScoringAdviceTwo.get_scoring_advice("aaaaa", "bbbbb", "cccc")).to eq(desired)
-    end
-
-    it"Expect a bad resource string" do
-      allow(Unirest).to receive(:get).and_return(status_404)
-      desired = "Something went wrong, The resource you requested could not be found."
-      expect(ScoringAdviceTwo.get_scoring_advice(nil, nil, nil)).to eq(desired)
-    end
-  end
-
-  private
-  def status_200
-    {
-      "body"
-        {
-          "propensity": 0.26532,
-          "ranking": "C"
-        },
-      "code": 200,
-      "headers": {}
-    }
-  end
-
-  def status_400
-    {
-      "body":
-       {
-         "status_code": 7,
-         "status_message": "Bad Request."
-       },
-      "code": 400,
-      "headers": {}
-    }
-  end
-
-  def status_404
-  {
-    "body":
-     {
-       "status_code": 34,
-       "status_message": "The resource you requested could not be found."
-     },
-   "code": 404,
-   "headers": {}
-  }
-  end
-
 end
--- spec/scoring_advice_spec.rb
+++ spec/scoring_advice_spec.rb
@@ -1,16 +1,63 @@
 # -*- encoding: utf-8 -*-
-require_relative '../lib/scoring_advice'
+require_relative '../lib/scoring_advice_two'
 
 describe ScoringAdvice do
-  describe "#ul" do
+  describe "#get_scoring_advice" do
 
-    it ":expect create an unordered list" do
-      ScoringAdvice.ul(['foo', 'bar']).should eql "<ul><li>foo</li><li>bar</li></ul>"
+    it"Expect sucessful call and have the desired data returned" do
+      allow(Unirest).to receive(:get).and_return(status_200)
+      desired = {"propensity": 0.26532, "ranking": "C"}
+      expect(ScoringAdviceTwo.get_scoring_advice(10000, 60657, 48)).to eq(desired)
     end
 
-    it ":expect escape appropriate characters" do
-      ScoringAdvice.ul(['<élan>']).should match %r(&lt;&eacute;lan&gt;)
+    it"Expect a Bad Request string" do
+      allow(Unirest).to receive(:get).and_return(status_400)
+      desired = "Something went wrong, Bad Request."
+      expect(ScoringAdviceTwo.get_scoring_advice("aaaaa", "bbbbb", "cccc")).to eq(desired)
     end
 
+    it"Expect a bad resource string" do
+      allow(Unirest).to receive(:get).and_return(status_404)
+      desired = "Something went wrong, The resource you requested could not be found."
+      expect(ScoringAdviceTwo.get_scoring_advice(nil, nil, nil)).to eq(desired)
+    end
   end
+
+  private
+  def status_200
+    {
+      "body":
+        {
+          "propensity": 0.26532,
+          "ranking": "C"
+        },
+      "code": 200,
+      "headers": {}
+    }
+  end
+
+  def status_400
+    {
+      "body":
+       {
+         "status_code": 7,
+         "status_message": "Bad Request."
+       },
+      "code": 400,
+      "headers": {}
+    }
+  end
+
+  def status_404
+  {
+    "body":
+     {
+       "status_code": 34,
+       "status_message": "The resource you requested could not be found."
+     },
+   "code": 404,
+   "headers": {}
+  }
+  end
+
 end
